7512c9b8cfe558dcdd1a3c5fee02d687de3afac4
Melissa Ribeiro <86805880+MelRibeiro@users.noreply.github.com>
2021-11-18 16:21:22 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,97 @@
+let canvas = document.getElementById("snake")
+let context = canvas.getContext("2d")
+let box = 32
+let snake = []
+snake[0] = {
+    x: 8 *  box,
+    y: 8 * box
+}
+let direction = "right"
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
+
+let pointsGame = document.getElementById("points")
+
+
+function createBG() {
+    context.fillStyle = "lightgreen" //aqui definidos a cor
+    context.fillRect(0, 0, 16 * box, 16 * box) //desenha o campo onde acontecerá o jogo
+}
+
+function createSnake () {
+    for(let i=0; i < snake.length; i++) {
+        context.fillStyle = 'green'
+        context.fillRect(snake[i].x, snake[i].y, box, box)
+    }
+}
+
+
+function drawFood() {
+    context.fillStyle = 'red'
+    context.fillRect(food.x, food.y, box, box)
+}
+
+
+document.addEventListener('keydown', update)
+
+function update(event) {
+    if (event.keyCode === 37 && direction !== "right") direction = "left"
+    if (event.keyCode === 38 && direction !== "down") direction = "up"
+    if (event.keyCode === 39 && direction !== "left") direction = "right"
+    if (event.keyCode === 40 && direction !== "up") direction = "down"
+}
+
+function startGame() {
+    if (snake[0].x > 15 * box && direction === "right") snake[0].x = 0
+    if (snake[0].x < 0 && direction === "left") snake[0].x = 16 * box
+    if (snake[0].y > 15 * box && direction === "down") snake[0].y = 0
+    if (snake[0].y < 0 && direction === "up") snake[0].y = 16 * box
+
+    for(i = 1; i < snake.length; i++) {
+        if (snake[0].x === snake[i].x && snake[0].y === snake[i].y) {
+            clearInterval(game)
+            alert("Game Over")
+        }
+    }
+
+
+    createBG()
+    createSnake()
+    drawFood()
+    showScoreOnScreen()
+
+    //para a cobra ter um ponto de partida, é necessário setar o eixo X e Y
+    let snakeX = snake[0].x
+    let snakeY = snake[0].y
+
+    if (direction === "right") snakeX += box;
+    if (direction === "left") snakeX -= box;
+    if (direction === "up") snakeY -= box;
+    if (direction === "down") snakeY += box;
+
+    if(snakeX !== food.x || snakeY !== food.y) {
+        snake.pop()
+    }
+    else{
+        food.x =Math.floor(Math.random() * 15 + 1) * box
+        food.y = Math.floor(Math.random() * 15 + 1) * box
+    }
+
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead)
+
+}
+ let game = setInterval(startGame, 100)
+
+
+function showScoreOnScreen () {
+    pointsGame.innerHTML = snake.length
+    }
+
